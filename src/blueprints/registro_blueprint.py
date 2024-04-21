774ae2e3220154404f666d043403c83b8fdbbf85
AlexB <a.benitezm@uniandes.edu.co>
2024-04-21 10:02:25 -0500

    obtener planes con beneficios

--- src/blueprints/registro_blueprint.py
+++ src/blueprints/registro_blueprint.py
@@ -4,7 +4,7 @@ from src.commands.registro.actualizar_plan_subscripcion import ActualizarPlanSub
 from src.commands.registro.obtener_deportista import ObtenerDeportista
 from src.commands.registro.registrar_deportista import RegistrarDeportista
 from src.commands.registro.registrar_deporte_deportista import RegistrarDeporteDeportista
-from src.commands.registro.obtener_plan_subscripcion import ObtenerPlanSubscripcion
+from src.commands.registro.obtener_plan_subscripcion import ObtenerPlanSubscripcion, ObtenerPlanesSubscripcion
 from src.commands.registro.registrar_socios import RegistrarSocios
 from src.utils.seguridad_utils import DeportistaToken, token_required
 
@@ -20,6 +20,12 @@ def obtener_deportista(deportista_token: DeportistaToken):
         'email': deportista_token.email,
     }
     result = ObtenerDeportista(**info_deportista).execute()
+    return make_response(jsonify(result), 200)
+
+
+@registro_blueprint.route('/obtener_planes_subscripion', methods=['GET'])
+def obtener_planes_subscripcion():
+    result = ObtenerPlanesSubscripcion().execute()
     return make_response(jsonify(result), 200)
 
 
--- src/commands/registro/obtener_plan_subscripcion.py
+++ src/commands/registro/obtener_plan_subscripcion.py
@@ -2,9 +2,11 @@ import os
 import jwt
 import logging
 from src.commands.base_command import BaseCommand
+from src.models.detalle_subscripcion import DetalleSubscripcion
 from src.models.plan_subscripcion import PlanSubscripcion
 from src.models.db import db_session
 from src.errors.errors import BadRequest, UserAlreadyExist
+from src.utils.str_utils import str_none_or_empty
 
 logger = logging.getLogger(__name__)
 
@@ -24,3 +26,41 @@ class ObtenerPlanSubscripcion(BaseCommand):
             raise BadRequest
         else:
             return plan.id
+
+
+class ObtenerPlanesSubscripcion(BaseCommand):
+    def __init__(self):
+        pass
+
+    def execute(self):
+        logger.info(f"Buscando Planes de Subscripcion")
+        respuesta = []
+        obtenerPlanesSubscripcion  = db_session.query(PlanSubscripcion).all()
+        if obtenerPlanesSubscripcion is None:
+            logger.error("Planes no encontrados")
+            raise BadRequest
+        else:
+            for planes in obtenerPlanesSubscripcion:
+                beneficios = db_session.query(DetalleSubscripcion).filter(DetalleSubscripcion.id_plan_subscripcion == planes.id).first()
+
+                if beneficios is None:
+                    logger.error("Beneficios no encontrados")
+                    raise BadRequest
+                else:
+                    split_beneficios = beneficios.beneficios.split('|')
+                    beneficiosOrder = []
+                    for i in range(len(split_beneficios)):
+                        beneficios_temp = {
+                            'beneficios': split_beneficios[i],
+                            'id_detalle_subscripcion': i+1
+                        }
+                        beneficiosOrder.append(beneficios_temp)
+
+                    resp_tmp = {
+                        'id_plan_subscripcion': planes.id,
+                        'nombre': planes.nombre,
+                        'beneficios': beneficiosOrder
+                    }
+                    respuesta.append(resp_tmp)
+            return respuesta       
+
